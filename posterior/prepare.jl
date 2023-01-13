b0d702ab0cd45c71830e4a5c1f118f7b66558074
zeio <zeionara@gmail.com>
2023-01-13 21:37:48 +0300

    Added progress bar on resizing images from the Term.jl package

--- posterior/prepare.jl
+++ posterior/prepare.jl
@@ -3,22 +3,28 @@
 import YAML
 
 using FileIO, Images
+using Term.Progress
 
 include("movie.jl")
+
+println("Loading input file...")
 
 data = YAML.load_file("assets/movies.yml")
 movies = (movie -> Movie(movie["title"], movie["poster"], movie["rating"], movie["year"], movie["id"])).(data["items"])
 
+progress_bar = ProgressBar(; columns = :detailed)
+job = addjob!(progress_bar; N = size(movies, 1), description = "resizing posters")
+
 function resize(movie :: Movie, width :: Int64 = 256, height :: Int64 = 256)
     poster = movie |> get_poster_local_path |> load
     save(get_poster_local_path(movie, "assets/posters/resized"), imresize(poster, width, height))
+
+    update!(job)
+    render(progress_bar)
 end
 
-resize.(movies)
+println("Resizing images...")
 
-# poster_paths = (movie -> movie |> get_poster_local_path).(movies)
-# 
-# poster = poster_paths |> first |> load
-# resized_poster = imresize(poster, 256, 256)
-# 
-# print(size(resized_poster))
+start!(progress_bar)
+resize.(movies)
+stop!(progress_bar)
