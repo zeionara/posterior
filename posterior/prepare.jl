4bc9ae5fa0c6824a49fe153c3be32ebc5062292d
zeio <zeionara@gmail.com>
2023-01-13 22:28:59 +0300

    Trying to add spinner

--- posterior/prepare.jl
+++ posterior/prepare.jl
@@ -3,21 +3,49 @@
 import YAML
 
 using FileIO, Images
-using ProgressBars
+using ProgressMeter
 
 include("movie.jl")
 
-println("Loading input file...")
+# println("Loading input file...")
 
+# spinner = ProgressUnknown("reading movies:", showspeed = true, spinner = true)
+
+# spin = true
+# @async while true
+#     print(".")
+#     sleep(0.05)
+# end
+
+# prog = ProgressUnknown("reading movies", spinner = true)
+# 
+# @async while true
+#     ProgressMeter.next!(prog)
+#     sleep(0.1)
+# end
+# 
+sleep(1)
+# 
+# ProgressMeter.finish!(prog)
+
+print(".")
 data = YAML.load_file("assets/movies.yml")
 movies = (movie -> Movie(movie["title"], movie["poster"], movie["rating"], movie["year"], movie["id"])).(data["items"])
+print(".")
 
-width = 256
-height = 256
+spin = false
 
-println("Resizing images...")
-
-for movie in ProgressBar(movies)
+function resize(movie :: Movie, width :: Int64 = 256, height :: Int64 = 256)
     poster = movie |> get_poster_local_path |> load
     save(get_poster_local_path(movie, "assets/posters/resized"), imresize(poster, width, height))
+
+    # next!(spinner)
+    # next!(progress_bar)
 end
+
+# finish!(spinner)
+# progress_bar = Progress(size(movies, 1), desc = "resizing images:", showspeed = true)
+resize.(movies)
+
+
+
