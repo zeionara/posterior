88e87f95cd3056d5398dae8cb7559f003b07130b
dima <zeionara@gmail.com>
2023-01-30 21:02:23 +0000

    Implemented trained model loading

--- posterior/serve.jl
+++ posterior/serve.jl
@@ -1,8 +1,37 @@
 #!/usr/bin/julia
 
+using ArgParse
 using Genie
+using Flux
+
+using BSON: @load
 
 Genie.config.run_as_server = true
+
+function parse_arguments()
+    settings = ArgParseSettings()
+
+    @add_arg_table settings begin
+        "--model-path", "-m"
+            help = "Trained model path"
+            arg_type = String
+            default = "assets/model.bson"
+        "--cpu", "-c"
+            help = "Run model on cpu"
+            action = :store_true
+    end
+
+    return parse_args(settings)
+end
+
+args = parse_arguments()
+
+# println(args["model-path"])
+device = args["cpu"] ? cpu : gpu
+
+@load args["model-path"] model_at_cpu
+
+model = model_at_cpu |> device
 
 route("/predict", method = POST) do
   message = Genie.Requests.jsonpayload()
